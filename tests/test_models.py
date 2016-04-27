29cc84f59cbb26a0170834eb87f1fb058f5a9ace
jmavila <jm.avila@bi4group.com>
2016-04-27 21:06:28 +0200

    Initial structure with basic behave example

---
+++ tests/test_models.py
@@ -0,0 +1,8 @@
+"""Testing file for models.py"""
+
+import unittest
+
+class TestModels(unittest.TestCase):
+    def test_example(self):
+        print "test example"
+        self.assertTrue(1 == 1)
