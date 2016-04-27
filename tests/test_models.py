88943d95eca8e2f495d0b199cceedf267e156338
jmavila <jm.avila@bi4group.com>
2016-04-27 21:21:15 +0200

    testing integration travis

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,3 +6,6 @@ class TestModels(unittest.TestCase):
     def test_example(self):
         print "test example"
         self.assertTrue(1 == 1)
+
+    def test_example2(self):
+        self.assertTrue(2 == 1)
